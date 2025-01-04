c988ffbeaecd79f26ad9b66c6800fe8d6ee786dd
alfaashu <alfaashu@outlook.com>
2025-01-04 07:11:19 +0530

    Fetch impelemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,22 @@
 import RestaurantCard from "./RestaurantCard"
 import resList from "../utils/mockData"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const Body = () => {
 
-    // Local State Variable - Super powerful variable
     const [listOfRestaurant, setListOfRestaurant] = useState(resList)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async() => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5974385&lng=77.3826845&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json()
+        console.log(json);
+        setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    }
 
     return (
         <div className="body">
