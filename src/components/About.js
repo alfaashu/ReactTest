604998d71623bfa510595e23021d318c8a726fc6
alfaashu <alfaashu@outlook.com>
2025-01-28 19:16:14 +0530

    Class based component & Props

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User"
+import UserClass from "./UserClass"
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>Namaste React web series</h2>
+            <User name={"Akshay Saini (function)"} />
+            <UserClass name={"Akshay Saini (Class)"} location={"Dehradun Class"}/>
         </div>
     )
 }
