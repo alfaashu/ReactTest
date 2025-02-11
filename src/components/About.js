72a69d1bf2270f16c4ef106a704b0d01eeb97ec1
alfaashu <alfaashu@outlook.com>
2025-02-11 20:23:06 +0530

    LifeCycle of a component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,28 @@
 import User from "./User"
 import UserClass from "./UserClass"
+import React from "react"
 
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
+class About extends React.Component{
+    constructor(props) {
+        super(props) 
+    }
+
+    componentDidMount(){
+        console.log("Child compoenet did mount");
+        
+    }
+
+    render() {
+        
+        return(
+            <div>
+            <h1>About Class Components</h1>
             <h2>Namaste React web series</h2>
-
             <UserClass name={"Akshay Saini (Class)"} location={"Dehradun Class"}/>
         </div>
-    )
+        )
+    }
 }
+
 
 export default About
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,32 +6,37 @@ class UserClass extends React.Component {
         super(props)
 
         this.state = {
-            count: 0,
-            count2: 2,
+            userInfo:{
+                name: "Dummy",
+                location: "Default"
+            }
         }
-        
     }
+
+    async componentDidMount(){
+        const data = await fetch("https://api.github.com/users/alfaashu")
+        const json = await data.json()
+
+        this.setState({
+            userInfo: json
+        })
+        console.log(json);
+    }
+
 
     render() {
 
-        const {name, location} = this.props
-        const {count} = this.state
-
+        const {name, location, avatar_url} = this.state.userInfo
+        
         return (
             <div className="user-card">
-                <h1>Count: {count}</h1>
-                <button  onClick={()=> {
-                    this.setState({
-                        count: this.state.count + 1,
-                    })
-                }}>Count Increase</button>
+                <img src={avatar_url}/>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: @akshaymarch7</h4>
             </div>
         ) 
     }
-
 }
 
 export default UserClass
