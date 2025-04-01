d4024d2bf7ddd3509e73a68f12cfe8acefda91c7
alfaashu <alfaashu@outlook.com>
2025-04-01 17:20:30 +0530

    Dynamic Import/Lazy Loading/Chunking/Bundling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react"
-import ReactDOM, { createRoot } from "react-dom/client"
+import React, {lazy, Suspense} from "react"
+import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router"
-import About from "./components/About"
+// import About from "./components/About"
 import Contact from "./components/Contact"
 import Error from "./components/Error"
 import RestaurantMenu from "./components/RestaurantMenu"
+import Shimmer from "./components/Shimmer"
+// import Grocery from "./components/Grocery"
 
 
-const elem = <span> Element</span>
-
+const Grocery = lazy(()=>import("./components/Grocery"))
+const About = lazy(()=>import("./components/About"))
 
 
 const AppLayout = () => {
@@ -33,11 +35,15 @@ const appRouter = createBrowserRouter([
             },
             {
                 path: "/about",
-                element: <About/>,
+                element: <Suspense><About/></Suspense>,
             },
             {
                 path: "/contact",
                 element: <Contact/>,
+            },
+            {
+                path: "/grocery",
+                element: <Suspense fallback={<Shimmer/>}><Grocery/></Suspense>,
             },
             {
                 path: "/restaurants/:resId",
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,10 +16,11 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Online Status: {onlineStatus? "🟢": "🔴"}</li>
+                    <li>Online Status: {onlineStatus? "🟢":"🔴"}</li>
                     <li> <Link to="/">Home</Link></li>
                     <li><Link to= "/about">About Us</Link></li>
                     <li><Link to= "/contact">Contact Us</Link></li>
+                    <li><Link to= "/grocery">Grocery</Link></li>
                     <li>Cart</li>
                     <button className="login" onClick={()=>{ btnNameReact === "Login" ?setBtnNameReact("Logout") : setBtnNameReact("Login")}}>{btnNameReact}</button>
                 </ul>
