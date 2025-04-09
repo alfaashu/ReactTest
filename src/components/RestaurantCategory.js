f14d093039f13faf03dbdbc32ed62b5f2194c93e
alfaashu <alfaashu@outlook.com>
2025-04-10 04:27:34 +0530

    Controlled and Uncontrolled components

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({data}) => {
-
-    const [showItems, setShowitems] = useState(false) 
-
+const RestaurantCategory = ({data, showItems, setShowIndex}) => {
     const handleClick = () => {
-        setShowitems(!showItems)
+        setShowIndex()
     }
-    
     return <div>
         {/* {Accordion Header} */}
         <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -21,16 +21,18 @@ const RestaurantMenu = () => {
     cards.find((c) => c?.card?.card?.itemCards)?.card?.card?.itemCards || [];
     
     const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=>c.card?.card?.["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory") 
-    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=>c.card?.card?.["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory") 
 
     return (
         <div className="text-center">
             <h1 className="font-bold my-10 text-2xl">{name}</h1>
             <p className="font-bold text-lg">{cuisines.join(", ")} - {costForTwoMessage}</p>
             {/* {Categories accordions} */}
-            {categories.map((category)=> (
-                <RestaurantCategory key = {category?.card?.card.title} data={category?.card?.card}
-                showItems={false}/>
+            {categories.map((category, index)=> (
+                // controlled components
+                <RestaurantCategory key = {category?.card?.card.title} 
+                data={category?.card?.card}
+                showItems={index === showIndex ? true : false}
+                setShowIndex={() => setShowIndex(index)}/>
             ))}
         </div>
     )
