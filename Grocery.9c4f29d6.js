08e8a35d677f0fcc6a02a3007739c3d006edb172
alfaashu <alfaashu@outlook.com>
2025-04-16 07:22:07 +0530

    Updates

---
+++ Grocery.9c4f29d6.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire1a0f;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),l=()=>(0,a.jsx)("h1",{children:"Our grocery online store, and we have a lot of child components"})});
+//# sourceMappingURL=Grocery.9c4f29d6.js.map
